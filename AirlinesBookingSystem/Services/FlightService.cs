c4e9f66b6e9d4d1ed982bba5abffb98c19dae4c2
jean-jensen <JeanJensen911@gmail.com>
2026-05-18 20:16:01 +0700

    flightService emits event to update mongo

--- AirlinesBookingSystem/Services/FlightService.cs
+++ AirlinesBookingSystem/Services/FlightService.cs
@@ -1,11 +1,13 @@
 ﻿using AirlinesBookingSystem.DTOs.Create;
+using AirlinesBookingSystem.Interfaces;
 using AirlinesBookingSystem.Interfaces.Repositories;
 using AirlinesBookingSystem.Interfaces.Services;
 using AirlinesBookingSystem.Models;
+using Shared.Events;
 
 namespace AirlinesBookingSystem.Services;
 
-public class FlightService(IFlightRepository repo) : IFlightService
+public class FlightService(IFlightRepository repo, IAirlineClient client) : IFlightService
 {
     public async Task<List<Flight>> GetAllFlights()
     {
@@ -31,8 +33,20 @@ public class FlightService(IFlightRepository repo) : IFlightService
     {
         var newFlight = CreateFlightDto.ToFlight(flight);
         await repo.AddFlight(newFlight);
-        
-        
+
+        var command = new MongoAddFlightCommand
+        {
+            Id = newFlight.Id,
+            FlightNumber = newFlight.FlightNumber,
+            OriginAirport = newFlight.OriginAirport,
+            DestinationAirport = newFlight.DestinationAirport,
+            DepartureTime = newFlight.DepartureTime,
+            ArrivalTime = newFlight.ArrivalTime,
+            AircraftId = newFlight.AircraftId,
+            Status = newFlight.Status,
+            Currency = newFlight.Currency
+        };
+        await client.Publish<MongoAddFlightCommand>(command);
     }
     
     public async Task UpdateFlight(Flight flight)
